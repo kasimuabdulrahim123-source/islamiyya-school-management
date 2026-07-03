b34d78b12c1d0d8d28d1651f4f16e9244c304329
kasimuabdulrahim123-source <kasimu.abdulrahim.123@gmail.com>
2026-07-03 20:58:46 +0100

    Create StaffPage component for staff information

---
+++ app/staff/page.tsx
@@ -0,0 +1,8 @@
+export default function StaffPage() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>👩‍🏫 Our Staff</h1>
+      <p>Staff page is working successfully!</p>
+    </main>
+  );
+}
